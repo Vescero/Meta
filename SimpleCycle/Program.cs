1acbea9647b8e57cf8d171b255a3a81d20003783
Александр Могильный <blagimi@yandex.ru>
2021-05-24 21:43:38 +0500

    2.13

--- SimpleCycle/Program.cs
+++ SimpleCycle/Program.cs
@@ -10,7 +10,11 @@ namespace SimpleCycle
             //BasicInfo.SimpleDo();
             //BasicInfo.SimpleWhile();
             //BasicInfo.SimpleBreak();
-            BasicInfo.SimpleContinue();
+            //BasicInfo.SimpleContinue();
+            //Extra.Bank();
+            //Extra.BankWhile();
+            //Extra.Multi();
+            Extra.MultiTable();
         }
     }
     public class BasicInfo
@@ -67,7 +71,75 @@ namespace SimpleCycle
     {
         public static void Bank()
         {
+            Console.WriteLine("Введите сумму вклада");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("На сколько месяц оформляется вклад?");
+            int months = Convert.ToInt32(Console.ReadLine());
+            double percent = 0.07;
+            Console.WriteLine($"Сумма вклада {sum} \n Колличество месяцев {months}");
+            for (int i = 0; i<months; i++)
+            {
+                sum +=sum * (decimal)percent;
+                Console.WriteLine(sum);
+            }
+        }
 
+        public static void BankWhile()
+        {
+            Console.WriteLine("Введите сумму вклада");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("На сколько месяц оформляется вклад?");
+            int months = Convert.ToInt32(Console.ReadLine());
+            double percent = 0.07;
+            Console.WriteLine($"Сумма вклада {sum} \n Колличество месяцев {months}");
+            int i = months;
+            while (i>0)
+            {
+                sum += sum * (decimal)percent;
+                i--;
+                Console.WriteLine(sum);
+            }
+        }
+
+        public static void Multi()
+        {
+            int number1, number2, sum;
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                //number1 = Convert.ToInt32(Console.ReadLine());
+                while(!int.TryParse(Console.ReadLine(),out number1))
+                {
+                    Console.WriteLine("Ошибка ввода! Введите целое число");
+                }
+                
+                Console.WriteLine("Введите второе число");
+                while (!int.TryParse(Console.ReadLine(), out number2))
+                {
+                    Console.WriteLine("Ошибка ввода! Введите целое число");
+                }
+
+                if (number1<0 || number1>10 || number2<0 || number2>10)
+                {
+                    Console.WriteLine("Числа должные быть больше 0 и меньше 10");
+                }
+                else
+                {
+                    sum = number1 * number2;
+                    Console.WriteLine($"Результат умножения двух чисел: {number1} и {number2} = {sum}");
+                }
+            }
+        }
+        
+        public static void MultiTable()
+        {
+            for (int x = 1; x<10; x++)
+            {
+                for (int y=1; y<10; y++)
+                {
+                    Console.Write($"{x*y} \t");
+                }
+            }
         }
     }
 }
