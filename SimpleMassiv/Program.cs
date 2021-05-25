f255c452b5ad9a6e6a20a19fd794e838b77d66c1
Александр Могильный <blagimi@yandex.ru>
2021-05-26 00:27:13 +0500

    chapter 2 13.2

--- SimpleMassiv/Program.cs
+++ SimpleMassiv/Program.cs
@@ -11,6 +11,9 @@ namespace SimpleMassiv
             //Basic.MassForExtra();
             //Basic.OneAndTwoDimmMassive();
             //Basic.ZubMassive();
+            //Basic.PositiveNumbers();
+            //Basic.MassInverse();
+            Extra.OutputMass();
         }
     }
     class Basic
@@ -97,9 +100,67 @@ namespace SimpleMassiv
             }
             Console.ReadKey();  
         }
+        public static void PositiveNumbers()
+        {
+            int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+            int result = 0;
+            foreach (int number in numbers)
+            {
+                if (number>0)
+                {
+                    result++;
+                    Console.Write($"{number} \t");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Число элементов больше 0: {result}");
+            Console.ReadKey();
+        }
+        public static void MassInverse()
+        {
+            int[] numbers = { -4, -3, -2, -1, 0, 5, 6, 7, 8 };
+            int n = numbers.Length;
+            int k = n/2;
+            int temp;
+            for (int i=0;i<k;i++)
+            {
+                temp = numbers[i];
+                numbers[i] = numbers[n - i - 1];
+                numbers[n - i - 1] = temp;
+            }
+            foreach (int i in numbers)
+            {
+                Console.Write($"{i}\t");
+            }
+        }
     }
     class Extra
     {
-
+        public static void OutputMass()
+        {
+            int[,,] mas = { { { 1, 2},{ 3, 4 } },
+                            { { 4, 5},{ 6, 7} },
+                            { { 7, 8},{ 9, 10} },
+                            { { 10, 11},{12, 13} }
+            };
+            Console.Write("{");
+            for (int rows = 0 ; rows<mas.GetLength(0); rows++)
+            {
+                Console.Write("{");
+                for (int columns=0; columns<mas.GetLength(1); columns++)
+                {
+                    Console.Write("{ ");
+                    for (int dimension=0; dimension<mas.GetLength(2);dimension++)
+                    {
+                        Console.Write(mas[rows,columns,dimension] + ",");
+                    }
+                    Console.Write("\b },");
+                }
+                Console.Write("\b},");
+            }
+            Console.Write("\b}");
+            Console.ReadKey();
+        }
     }
 }
