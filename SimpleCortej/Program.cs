ef56d747f39f13ce7aef0d2b02c581177b83b71d
Александр Могильный <blagimi@yandex.ru>
2021-05-30 21:53:30 +0500

    Chapter 2.22

---
+++ SimpleCortej/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SimpleCortej
+{
+    class Program
+    {
+        static void Main()
+        {
+            var turple = (5, 10);// Кортеж с неявно указанным типом
+            Console.WriteLine(turple.Item1);//5
+            Console.WriteLine(turple.Item2);//10
+            turple.Item1 += 26;
+            Console.WriteLine(turple.Item1);//31
+
+            (int,int) turple2 = (5, 10);// Кортеж с явным указанием типа
+
+            (string, int, double) person = ("Tom", 25, 81.23);
+
+            var turple3 = (count: 5, sum: 10);  //Можно дать название полям
+            var (name, age) = ("Tom", 23);  // Можно не использовать переменную а поля
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(turple3.count);
+
+            var turpleM = GetValues();
+            Console.WriteLine(turpleM.Item1);   // 1
+            Console.WriteLine(turpleM.Item2);   // 3
+            Console.WriteLine();
+
+            // Кортежу присваивается результат работы метода в который передаётся массив
+            var turple4 = GetNamedValues(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+            Console.WriteLine(turple4.count);
+            Console.WriteLine(turple4.sum);
+            Console.WriteLine();
+
+            var (name2, age2) = GetTurple(("Tom", 23), 12);
+            Console.WriteLine(name2);   // Tom
+            Console.WriteLine(age2);    // 35
+
+            Console.ReadKey();
+        }
+        /* Метод возвращает кортеж для возвращения 2х и более значений */
+        private static (int,int) GetValues()    
+        {
+            var result = (1, 3);
+            return result;
+        }
+        /* Метод возвращает кортеж с суммой значений массива и количества итераций */
+        private static (int sum,int count) GetNamedValues(int[] numbers)
+        {
+            // Создаётся кортеж 
+            var result = (sum: 0, count: 0);
+            for (int i=0;i<numbers.Length;i++)
+            {
+                result.sum += numbers[i];
+                result.count++;
+            }
+            return result;
+        }
+        /* Кортеж может передаваться в качестве параметрва в метод */
+        private static (string name, int age) GetTurple((string n, int a) turple, int x)
+        {
+            var result = (name: turple.n, age: turple.a + x);
+            return result;
+        }
+    }
+}
---
+++ SimpleEnum/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace SimpleEnum
+{
+    enum GlobalOperation
+    {
+        Add,
+        Substract,
+        Multiply,
+        Divide
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Operation op = new Operation();
+            op = Operation.Add;
+            Console.WriteLine(op);
+            op = Operation.Multiply;    // Привидение к числовому типу
+            Console.WriteLine((int)op); // для вывода значения переменной
+            MathOp(10, 5, Operation.Add);
+            MathOp(11, 5, Operation.Multiply);
+            Console.ReadKey();
+        }
+        /* Метод принимает два числовых аргумента и тип операции */
+        static void MathOp(double x,double y,Operation operation)
+        {
+            double result = 0.0;
+            switch (operation)
+            {
+                case Operation.Add:
+                    result = x + y;
+                    break;
+                case Operation.Substract:
+                    result = x - y;
+                    break;
+                case Operation.Multiply:
+                    result = x * y;
+                    break;
+                case Operation.Divide:
+                    result = x / y;
+                    break;
+            }
+            Console.WriteLine($"Результат: {result}");
+        }
+        enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        enum Time : byte
+        {
+            Morning,
+            Afternoon,
+            Evening,
+            Night
+        }
+        /* Зачастую переменная перечисления выступает в качестве хранилища состояния,
+         * в зависимости от которого производятся некоторые действия*/
+        public enum Operation
+        {
+            Add = 1,    // Каждый следующий элемент увеличится на 1
+            Substract,  // 2
+            Multiply,   // 3
+            Divide      // 4
+        }
+        enum Operation2
+        {
+            Add = 2,    // Можно явно указать значение всех элементов
+            Substract = 4,
+            Multiply = 8,
+            Divide = 16
+        }
+        enum Color
+        {
+            White = 1,  // Константы перечисления могут иметь одинаковые значение
+            Black = 2,  // и могут присваивать значения друг друга
+            Green = 2,
+            Blue = White // Blue = 1
+        }      
+    }
+}
